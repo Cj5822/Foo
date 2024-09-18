069f0457b5731b3d5cb7af90c9ad8587882c754d
ljun123@aucklanduni.ac.nz <ljun123@aucklanduni.ac.nz>
2024-09-19 02:37:52 +1200

    Created a flipped wrench controller to control what the icons do

---
+++ src/main/java/nz/ac/auckland/se206/controllers/FlippedWrenchController.java
@@ -0,0 +1,37 @@
+package nz.ac.auckland.se206.controllers;
+
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.shape.Rectangle;
+import nz.ac.auckland.apiproxy.exceptions.ApiProxyException;
+import nz.ac.auckland.se206.GameStateContext;
+
+public class FlippedWrenchController {
+  private static GameStateContext context = new GameStateContext();
+
+  @FXML private AnchorPane flippedWrenchPane;
+
+  @FXML
+  public void initialize() throws ApiProxyException {
+    // Any required initialization code can be placed here
+    hideFlippedWrenchPane(); // Hide wrench pane initially
+  }
+
+  @FXML
+  private void handleRectangleClick(MouseEvent event) throws IOException {
+    Rectangle clickedRectangle = (Rectangle) event.getSource();
+    context.handleRectangleClick(event, clickedRectangle.getId());
+  }
+
+  public void showFlippedWrenchPane() {
+    flippedWrenchPane.setVisible(true);
+  }
+
+  public void hideFlippedWrenchPane() {
+    flippedWrenchPane.setVisible(false);
+  }
+
+}

5e54df346a600ddfa1e302f65dc363c6dd170e08
miguel-bal <mbal759@aucklanduni.ac.nz>
2024-09-20 21:41:51 +1200

    added replay functionality at the end of the game

--- src/main/java/nz/ac/auckland/se206/controllers/RoomController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/RoomController.java
@@ -5,6 +5,7 @@ import javafx.animation.AnimationTimer;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
@@ -429,6 +430,16 @@ public class RoomController {
         lblRoomName.setText("Bedroom");
         return;
     }
+  }
+
+  @FXML
+  public void handleReplay(ActionEvent event) throws IOException {
+    Button button = (Button) event.getSource();
+    Scene gameScene = button.getScene();
+    gameScene.setRoot(App.loadFxml("homepage"));
+    timerManager = TimerManager.getInstance(context);
+    timerManager.reset();
+    startTimer();
   }
 
   /**
